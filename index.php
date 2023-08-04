adb23d0319b1cf4cc20c9b36e944bf47c3bc4370
SH20RAJ <66713844+SH20RAJ@users.noreply.github.com>
2023-08-04 05:06:03 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Article Planet</title>
+  <script src="https://unpkg.com/showdown/dist/showdown.min.js"></script>
+</head>
+<body>
+  <script>
+    var converter = new showdown.Converter(),
+    text      = '# hello, markdown!',
+    html      = converter.makeHtml(text);
+  </script>
+
+</body>
+</html>
